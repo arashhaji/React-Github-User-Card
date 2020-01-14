1102ef03d75437be904a62b6d49a7033af0a0b89
arashhaji <56742302+arashhaji@users.noreply.github.com>
2020-01-14 16:54:59 -0500

    mvp

--- my-github/src/components/FollowersCard.js
+++ my-github/src/components/FollowersCard.js
@@ -1,15 +1,16 @@
 import React from "react";
 
-const UserCard = ({user, img, bio}) => {
+const FollowersCard = ({followers, img, bio}) => {
     return (
         <div className="card">
 
             <img src={img} alt="user"/>
-            <p className="name">Name: {user}</p>
+            <p className="name">Name: {followers}</p>
             <p className="name">Bio: {bio}</p>
 
         </div>
     )
 }
 
-export default UserCard;
+export default FollowersCard;
+
